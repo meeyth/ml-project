a581d768a2eb4a65f23908f7559a77bae534ca96
meeyth <anurupbhowmick2003@gmail.com>
2024-09-09 01:58:39 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys
+from src.logger import logging
+
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+
+    error_message = f"Error occured in python script name [{file_name}] line number [{line_no}] error message [{str(error)}]"
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        res = 1/0
+
+    except Exception as e:
+        logging.info("Exception Zero Division")
+        raise CustomException(e, sys)
